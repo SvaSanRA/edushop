6467bce5e5adb74eb4974a88a31a0a64d49b439f
Alexander.Svetlov <svasanra@gmail.com>
2017-11-27 11:59:56 +0700

    Состояние проекта на конец 7-го урока.

---
+++ views/add_new_product.php
@@ -0,0 +1,15 @@
+<form action="" method="POST">
+    <div>
+        Название товара:
+        <input type="text" name="product_name" />
+    </div>
+    <div>
+        Описание товара:
+        <input type="text" name="product_description" />
+    </div>
+    <div>
+        Цена: <input type="text" name="price" />
+    </div>
+    <input type="submit" value="Добваить" />
+
+</form>
